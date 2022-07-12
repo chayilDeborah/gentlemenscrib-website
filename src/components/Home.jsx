125b4f33f70df925b388b655bb5ec4aa2defb47a
Orunto Eniola <98480650+orunto@users.noreply.github.com>
2022-07-12 02:42:39 +0100

    Routing completed for the current build

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-import { Footer } from "../components/Footer";
-import Navbar from "../components/Navbar";
-import Student from "./Student";
-// import Owanbe from '../components/Owanbe';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Thanks to the index.js file I wrote earlier you can call all the components in a single line
+import {Gym, Owanbe, Navbar, Owambe, TechBro, PackNav, WorkFromHome, Footer} from '../components' 
+
+import '../css/PackNav.css';
 // import Gym from "./Gym";
 // import TechBro from "./TechBro";
 // import WorkFromHome from "./WorkFromHome";
 
 const Home = () => {
     return (
-        <div>
+        <Router>
             <Navbar /> 
+            <Routes>
+                <Route path='/' element={<TechBro />}/>
+                <Route path='/owanbe' element={<Owanbe />}/>
+                <Route path='/gym' element={<Gym />}/>
+                <Route path='/workfromhome' element={<WorkFromHome />}/>
+            </Routes>
+
             {/* <TechBro /> */}
             {/* <WorkFromHome /> */}
             {/* <Gym /> */}
-            {/* <Owanbe /> */}
-            <Student />
 
             <PackNav />
 
             <Footer />
-        </div>
+        </Router>
     )
 }
 export default Home;
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export { default as Footer } from './Footer';
+export { default as Gym } from './Gym';
+export { default as Navbar } from './Navbar';
+export { default as Owanbe } from './Owanbe';
+export { default as PackNav } from './PackNav';
+export { default as TechBro } from './TechBro';
+export { default as WorkFromHome } from './WorkFromHome';
+
+// This index.js file allows me to call these components on a single line in Home.jsx

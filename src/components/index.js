e1dbad54f1e83198550ff7346d3e18d0e93a3e1f
Orunto Eniola <98480650+orunto@users.noreply.github.com>
2022-07-12 11:51:59 +0100

    Export considerations for the remaining pages

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,10 @@ export { default as Owanbe } from './Owanbe';
 export { default as PackNav } from './PackNav';
 export { default as TechBro } from './TechBro';
 export { default as WorkFromHome } from './WorkFromHome';
+export { default as CorporateCasual } from './CorporateCasual';
+export { default as DateNightSpecial } from './DateNightSpecial';
+export { default as PartyGoer } from './PartyGoer';
+export { default as RainingSeason } from './RainingSeason';
+export { default as Student } from './Student';
 
 // This index.js file allows me to call these components on a single line in Home.jsx
